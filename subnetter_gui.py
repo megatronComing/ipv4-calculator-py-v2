e7937ac631eda8d29501759b1c7f768930426855
megatronComing <114308295+megatronComing@users.noreply.github.com>
2024-04-05 10:17:31 -0400

    Update subnetter_gui.py

--- subnetter_gui.py
+++ subnetter_gui.py
@@ -9,7 +9,7 @@ class App(QWidget):
     
     def initUI(self):
         self.setWindowTitle('IPv4 subnetting calculator')
-        self.setGeometry(100, 100, 900, 400)
+        self.setGeometry(100, 100, 700, 400)
 
         # Main vertical layout
         mainLayout = QVBoxLayout(self)
